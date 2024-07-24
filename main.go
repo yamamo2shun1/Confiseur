46c5fdacb4e2462f3f24c03f4220ad60992977c4
Shunichi Yamamoto <shunichi@made-in-yamamoto.com>
2024-07-24 10:05:28 +0900

    Support rewriting of C4NDY STK.

--- main.go
+++ main.go
@@ -19,13 +19,17 @@ type Layouts struct {
 }
 
 type Layout struct {
-	Rows [][]string `toml:"rows"`
+	Normal [][]string `toml:"normal"`
+	Upper  [][]string `toml:"upper"`
 }
 
 var err error
 var d *hid.Device
 var layouts Layouts
-var remapRow []byte = make([]byte, 16)
+var remapRows []byte = make([]byte, 16)
+
+var maxRows = 5
+var maxColumns = 13
 
 func GetSettingPath() string {
 	path := "nopath"
@@ -55,6 +59,11 @@ func checkHid() {
 	}
 	fmt.Printf("Product String: %s\n", s)
 
+	if strings.Contains(s, "C4NDY STK") {
+		maxRows = 4
+		maxColumns = 10
+	}
+
 	// Read the Serial Number String.
 	s, err = d.GetSerialNbr()
 	if err != nil {
@@ -62,34 +71,36 @@ func checkHid() {
 	}
 	fmt.Printf("Serial Number String: %s\n", s)
 
-	// Read Indexed String 1.
-	s, err = d.GetIndexedStr(1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Indexed String 1: %s\n", s)
+	/*
+		// Read Indexed String 1.
+		s, err = d.GetIndexedStr(1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Indexed String 1: %s\n", s)
+	*/
 }
 
 func loadKeymap(val byte) {
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
 
-	for i := 0; i < 5; i++ {
-		remapRow[0] = 0x00
-		remapRow[1] = 0xF0 + byte(i)
-		remapRow[2] = val
-		if _, err := d.Write(remapRow); err != nil {
+	for i := 0; i < maxRows; i++ {
+		remapRows[0] = 0x00
+		remapRows[1] = 0xF0 + byte(i)
+		remapRows[2] = val
+		if _, err := d.Write(remapRows); err != nil {
 			log.Fatal(err)
 		}
 		time.Sleep(100 * time.Millisecond)
 
-		if _, err := d.Read(remapRow); err != nil {
+		if _, err := d.Read(remapRows); err != nil {
 			log.Fatal(err)
 		}
 		time.Sleep(100 * time.Millisecond)
 
 		fmt.Fprint(w, "[\t")
-		for j := 0; j < 13; j++ {
-			fmt.Fprintf(w, "%s\t", KN[remapRow[j]])
+		for j := 0; j < maxColumns; j++ {
+			fmt.Fprintf(w, "%s\t", KN[remapRows[j]])
 		}
 		fmt.Fprintln(w, "]\t")
 	}
@@ -97,19 +108,24 @@ func loadKeymap(val byte) {
 }
 
 func writeKeymap(val byte) {
-	for i := 0; i < 5; i++ {
-		remapRow[0] = 0x00
-		remapRow[1] = 0xF0 + byte(i)
-		remapRow[2] = val
-		for j := 0; j < 13; j++ {
-			if val == 0x01 {
-				remapRow[j+3] = SC[layouts.Layout1.Rows[i][j]]
-			} else if val == 0x02 {
-				remapRow[j+3] = SC[layouts.Layout2.Rows[i][j]]
+	for i := 0; i < maxRows; i++ {
+		remapRows[0] = 0x00
+		remapRows[1] = 0xF0 + byte(i)
+		remapRows[2] = val
+		for j := 0; j < maxColumns; j++ {
+			switch val {
+			case 0x01:
+				remapRows[j+3] = SC[layouts.Layout1.Normal[i][j]]
+			case 0x02:
+				remapRows[j+3] = SC[layouts.Layout1.Upper[i][j]]
+			case 0x03:
+				remapRows[j+3] = SC[layouts.Layout2.Normal[i][j]]
+			case 0x04:
+				remapRows[j+3] = SC[layouts.Layout2.Upper[i][j]]
 			}
 		}
 
-		if _, err := d.Write(remapRow); err != nil {
+		if _, err := d.Write(remapRows); err != nil {
 			log.Fatal(err)
 		}
 		time.Sleep(100 * time.Millisecond)
@@ -127,19 +143,39 @@ func remap() {
 		log.Fatal(err)
 	}
 
+	fmt.Println(layouts)
+
 	fmt.Println("::Layout1::")
-	for i := 0; i < 5; i++ {
-		fmt.Println(layouts.Layout1.Rows[i])
+	fmt.Println("  Normal ->")
+	for i := 0; i < maxRows; i++ {
+		fmt.Println(layouts.Layout1.Normal[i])
 	}
-	for i := 0; i < 5; i++ {
+	for i := 0; i < maxRows; i++ {
 		fmt.Print("[ ")
-		for j := 0; j < 13; j++ {
-			if SC[layouts.Layout1.Rows[i][j]] == 0x00 {
+		for j := 0; j < maxColumns; j++ {
+			if SC[layouts.Layout1.Normal[i][j]] == 0x00 {
 				if err := hid.Exit(); err != nil {
 					log.Fatal(err)
 				}
 			} else {
-				fmt.Printf("0x%02X ", SC[layouts.Layout1.Rows[i][j]])
+				fmt.Printf("0x%02X ", SC[layouts.Layout1.Normal[i][j]])
+			}
+		}
+		fmt.Println("]")
+	}
+	fmt.Println("  Upper ->")
+	for i := 0; i < maxRows; i++ {
+		fmt.Println(layouts.Layout1.Upper[i])
+	}
+	for i := 0; i < maxRows; i++ {
+		fmt.Print("[ ")
+		for j := 0; j < maxColumns; j++ {
+			if SC[layouts.Layout1.Upper[i][j]] == 0x00 {
+				if err := hid.Exit(); err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				fmt.Printf("0x%02X ", SC[layouts.Layout1.Upper[i][j]])
 			}
 		}
 		fmt.Println("]")
@@ -148,14 +184,25 @@ func remap() {
 
 	fmt.Println("")
 	fmt.Println("::Layout2::")
-	for i := 0; i < 5; i++ {
-		fmt.Println(layouts.Layout2.Rows[i])
+	fmt.Println("  Normal ->")
+	for i := 0; i < maxRows; i++ {
+		fmt.Println(layouts.Layout2.Normal[i])
 	}
-
-	for i := 0; i < 5; i++ {
+	for i := 0; i < maxRows; i++ {
 		fmt.Print("[ ")
-		for j := 0; j < 13; j++ {
-			fmt.Printf("0x%02X ", SC[layouts.Layout2.Rows[i][j]])
+		for j := 0; j < maxColumns; j++ {
+			fmt.Printf("0x%02X ", SC[layouts.Layout2.Normal[i][j]])
+		}
+		fmt.Println("]")
+	}
+	fmt.Println("  Upper ->")
+	for i := 0; i < maxRows; i++ {
+		fmt.Println(layouts.Layout2.Upper[i])
+	}
+	for i := 0; i < maxRows; i++ {
+		fmt.Print("[ ")
+		for j := 0; j < maxColumns; j++ {
+			fmt.Printf("0x%02X ", SC[layouts.Layout2.Upper[i][j]])
 		}
 		fmt.Println("]")
 	}
@@ -163,17 +210,17 @@ func remap() {
 }
 
 func saveToFlash() {
-	remapRow[0] = 0x00
-	remapRow[1] = 0xF5
-	if _, err := d.Write(remapRow); err != nil {
+	remapRows[0] = 0x00
+	remapRows[1] = 0xF5
+	if _, err := d.Write(remapRows); err != nil {
 		log.Fatal(err)
 	}
 	time.Sleep(100 * time.Millisecond)
 
-	if _, err := d.Read(remapRow); err != nil {
+	if _, err := d.Read(remapRows); err != nil {
 		log.Fatal(err)
 	}
-	if remapRow[1] == 0xF5 {
+	if remapRows[1] == 0xF5 {
 		fmt.Println("Finish.")
 	}
 }
@@ -190,9 +237,31 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Read the Product String.
+	s, err := d.GetProductStr()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if strings.Contains(s, "C4NDY STK") {
+		maxRows = 4
+		maxColumns = 10
+	}
+
 	switch os.Args[1] {
 	case "check":
 		checkHid()
+		fmt.Println("")
+	case "help":
+		fmt.Println("META OPTIONS")
+		fmt.Println("  help     Display this information.")
+		fmt.Println("  ver      Show the version of the tool installed.")
+		fmt.Println("  check    Show information on C4NDY KeyVLM/STK connected to PC/Mac.")
+		fmt.Println("  load     Show the current key names of the keyboard.")
+		fmt.Println("  remap    Write the keyboard with the keymap set in layouts.toml.")
+		fmt.Println("  remap -f <file>")
+		fmt.Println("           Write the keymap set in the specified .toml to the keydoad.")
+		fmt.Println("  save     Save the keymap written by \"-r or --remap\" to the memory area.")
 		fmt.Println("")
 	case "load":
 		initKN()
@@ -200,24 +269,32 @@ func main() {
 		// check current hardware layout
 		fmt.Println("--- Current Hardware Layout ScanCode ---")
 		fmt.Println("::Layout1::")
-		loadKeymap(0x03)
+		fmt.Println("  Normal ->")
+		loadKeymap(0x05)
+		fmt.Println("  Upper ->")
+		loadKeymap(0x06)
 		fmt.Println("")
 		fmt.Println("::Layout2::")
-		loadKeymap(0x04)
+		fmt.Println("  Normal ->")
+		loadKeymap(0x07)
+		fmt.Println("  Upper ->")
+		loadKeymap(0x08)
 		fmt.Println("")
 	case "remap":
 		remap()
 
-		fmt.Println("remap layout1&2")
+		fmt.Println("remap layout1&2(Normal/Upper)")
 
 		writeKeymap(0x01)
 		writeKeymap(0x02)
+		writeKeymap(0x03)
+		writeKeymap(0x04)
 		fmt.Println("")
 	case "save":
 		saveToFlash()
 		fmt.Println("")
 	case "ver":
-		fmt.Println("C4NDY KeyVLM Configurator v0.3!")
+		fmt.Println("C4NDY KeyConfigurator v0.4!")
 		fmt.Println("")
 	default:
 	}
